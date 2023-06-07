e0892ae927d7b613679cb03372985c09a44b7742
Florin Cirtina <florin.cirtina@gmail.com>
2023-06-07 21:07:02 +0300

    Automation project

---
+++ src/test/java/tests/SignInTest/SignInTest.java
@@ -0,0 +1,18 @@
+package tests.SignInTest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+public class SignInTest extends BaseTest {
+    private static final Logger LOG = LoggerFactory.getLogger(SignInTest.class);
+
+    @Test
+    public void checkIfSignInButtonIsDisplayed() {
+
+        LOG.info("Check 'Sign in' button");
+        Assert.assertTrue(signInPage.isSignInButtonDisplayed(), "Button is not displayed");
+    }
+}
