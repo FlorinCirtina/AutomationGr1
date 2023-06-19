0dd101079de77c1c8a18c561be9e1050c5fa30ab
Florin Cirtina <florin.cirtina@gmail.com>
2023-06-19 20:58:02 +0300

    Added Register logic

--- src/test/java/pages/RegisterPage/RegisterPage.java
+++ src/test/java/pages/RegisterPage/RegisterPage.java
@@ -1,5 +1,6 @@
 package pages.RegisterPage;
 
+import org.openqa.selenium.By;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pages.BasePage;
@@ -17,4 +18,66 @@ public class RegisterPage extends BasePage {
         }
         return instance;
     }
+
+    private By inputFirstName = By.xpath("//input[@ng-model='FirstName']");
+    private By inputLastName = By.xpath("//input[@ng-model='LastName']");
+    private By inputAddress = By.xpath("//textarea[@ng-model='Adress']");
+    private By inputEmailAddress = By.xpath("//input[@type = 'email']");
+    private By inputPhoneNumber = By.xpath("//input[@type = 'tel']");
+    private By maleGender = By.xpath("//input[@value='Male']");
+    private By femaleGender = By.xpath("//input[@value='FeMale']");
+    private By cricketCheckmark = By.id("checkbox1");
+    private By moviesCheckmark = By.id("checkbox2");
+    private By hockeyCheckmark = By.id("checkbox3");
+
+    public void typeInFirstName(String fName) {
+        LOG.info("Type in FirstName");
+        driver.findElement(inputFirstName).sendKeys(fName);
+    }
+
+    public void typeInLastName(String lName) {
+        LOG.info("Type in LastName");
+        driver.findElement(inputLastName).sendKeys(lName);
+    }
+
+    public void typeInAdress(String address) {
+        LOG.info("Type in Address");
+        driver.findElement(inputAddress).sendKeys(address);
+    }
+
+    public void typeInEmailAddress(String emailAddress) {
+        LOG.info("Type in Email Address");
+        driver.findElement(inputEmailAddress).sendKeys(emailAddress);
+    }
+
+    public void typeInPhoneUmber(String phoneNumber) {
+        LOG.info("Type in Phone NUmber");
+        driver.findElement(inputPhoneNumber).sendKeys(phoneNumber);
+    }
+
+    public void setMaleGender() {
+        LOG.info("Click the Male radio button");
+        driver.findElement(maleGender).click();
+    }
+
+    public void setFemaleGender() {
+        LOG.info("Click the Female  radio button");
+        driver.findElement(femaleGender).click();
+    }
+
+    public void setCricket() {
+        LOG.info("Set Cricket");
+        driver.findElement(cricketCheckmark).click();
+    }
+
+    public void seMovies() {
+        LOG.info("Set Movies");
+        driver.findElement(moviesCheckmark).click();
+    }
+
+    public void setHockey() {
+        LOG.info("Set Hockey");
+        driver.findElement(hockeyCheckmark).click();
+    }
+
 }
--- src/test/java/tests/RegisterTest/RegisterTest.java
+++ src/test/java/tests/RegisterTest/RegisterTest.java
@@ -6,11 +6,44 @@ import org.testng.annotations.Test;
 import pages.RegisterPage.RegisterPage;
 import tests.BaseTest;
 
+import static pages.BasePage.driver;
+import static pages.BasePage.getBaseUrl;
+
 public class RegisterTest extends BaseTest {
     private static final Logger LOG = LoggerFactory.getLogger(RegisterPage.class);
+    private String newUrl = getBaseUrl() + "Register.html";
+
+    String firstName = "Ion";
+    String lAstName = "Pop";
+    String adress = "Cluj, Strada Primaverii, Nt 407 J";
+    String email = "Automation7@domain.com";
+    String phone = "0748955748";
 
     @Test
     public void register() {
+        driver.get(newUrl);
+
+        LOG.info("Type in FullName");
+        registerPage.typeInFirstName(firstName);
+        registerPage.typeInLastName(lAstName);
+
+        LOG.info("Type in Address");
+        registerPage.typeInAdress(adress);
+
+        LOG.info("Type Email address");
+        registerPage.typeInEmailAddress(email);
+
+        LOG.info("Type Phone NUmber");
+        registerPage.typeInPhoneUmber(phone);
+
+        LOG.info("Select Gender");
+        registerPage.setMaleGender();
+        registerPage.setFemaleGender();
+
+        LOG.info("Select Hobbies");
+        registerPage.setCricket();
+        registerPage.seMovies();
+        registerPage.setHockey();
 
     }
 }
